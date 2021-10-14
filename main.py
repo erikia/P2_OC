79c590618833fd97b553da73c91799e2f83b0611
erikia <nobileisabelle.in@gmail.com>
2021-10-14 18:07:40 +0200

    test

--- main.py
+++ main.py
@@ -8,46 +8,43 @@ soup = BeautifulSoup(response.content, 'html.parser')
 
 
 def getdata(url):
-
-
-"""Fonction pour appeler et analyser une page du site"""
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-return soup
+    """Fonction pour appeler et analyser une page du site"""
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    return soup
 
 
 def get_book_data(soup):
+    """Fonction pour analyser les données d'un livre"""
+    print("Démarrage du scrapping")
 
+    title = soup.find("h1").get_text()
+    print(title)
 
-"""Fonction pour analyser les données d'un livre"""
-print("Démarrage du scrapping")
+    upc = soup.find_all("td")[0].get_text()
+    print(upc)
 
-title = soup.find("h1").get_text()
-print(title)
+    price_excluding_tax = soup.find_all("td")[2].get_text()
+    print(price_excluding_tax)
 
-upc = soup.find_all("td")[0].get_text()
-print(upc)
+    price_including_tax = soup.find_all("td")[3].get_text()
+    print(price_including_tax)
 
-price_excluding_tax = soup.find_all("td")[2].get_text()
-print(price_excluding_tax)
+    number_available = soup.find_all("td")[5].get_text()
+    print(number_available)
 
-price_including_tax = soup.find_all("td")[3].get_text()
-print(price_including_tax)
+    description = soup.find("article").find_all(
+        "p")[3].get_text().replace(";", "/").strip()
+    print(description)
 
-number_available = soup.find_all("td")[5].get_text()
-print(number_available)
+    category = soup.find("ul", {"class": "breadcrumb"}).find_all("li")[2].text
+    print(category)
 
-description = soup.find("article").find_all(
-    "p")[3].get_text().replace(";", "/").strip()
-print(description)
+    rating = soup.find(
+        "div", {"class": "col-sm-6 product_main"}).find_all("p")[2]["class"][1]
+    print(rating)
 
-category = soup.find("ul", {"class": "breadcrumb"}).find_all("li")[2].text
-print(category)
- rating = soup.find(
-      "div", {"class": "col-sm-6 product_main"}).find_all("p")[2]["class"][1]
-  print(rating)
-
-   for item in soup.find_all("img"):
+    for item in soup.find_all("img"):
         name_img = item['alt']
         link_img = item['src']
         print(name_img, link_img)
@@ -56,3 +53,5 @@ print(category)
 
 
 get_book_data(soup)
+
+"test"
