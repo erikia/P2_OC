d4878cf78c46dbae52a18252ef04f3509f82c0af
erikia <91466871+erikia@users.noreply.github.com>
2021-10-14 17:52:22 +0200

    change for loop into function

---
+++ main.py
@@ -0,0 +1,53 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+url = 'https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+
+# def getdata(url):
+"""Fonction pour appeler et analyser une page du site"""
+"""response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    return soup
+"""
+
+# Scrap Data
+
+
+def get_book_data(soup):
+    print("Démarrage du scrapping")
+
+    title = soup.find("h1").get_text()
+    print(title)
+
+    upc = soup.find_all("td")[0].get_text()
+    print(upc)
+
+    price_excluding_tax = soup.find_all("td")[2].get_text()
+    print(price_excluding_tax)
+
+    price_including_tax = soup.find_all("td")[3].get_text()
+    print(price_including_tax)
+
+    number_available = soup.find_all("td")[5].get_text()
+    print(number_available)
+
+    description = soup.find("article").find_all(
+        "p")[3].get_text().replace(";", "/").strip()
+    print(description)
+
+    category = soup.find("ul", {"class": "breadcrumb"}).find_all("li")[2].text
+    print(category)
+    rating = soup.find(
+        "div", {"class": "col-sm-6 product_main"}).find_all("p")[2]["class"][1]
+    print(rating)
+
+    for item in soup.find_all("img"):
+        name_img = item['alt']
+        link_img = item['src']
+        print(name_img, link_img)
+
+    print("Opération terminé")
