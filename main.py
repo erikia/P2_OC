dce1ca082d7e898cae393743155dbca77b48346b
erikia <nobileisabelle.in@gmail.com>
2021-10-15 17:11:31 +0200

    fonction get_categories_data terminé

--- main.py
+++ main.py
@@ -1,60 +1,72 @@
+from typing import Text
 import requests
 from bs4 import BeautifulSoup
-import os
+from csv import DictWriter
+from pathlib import Path
 
-url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+
+"""url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 
+"""
+
 
 def getdata(url):
-    """Fonction pour appeler et analyser une page du site"""
+    """Fonction pour se connecter au site """
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     return soup
 
 
-categories = []
+def get_categories_data(url_categories):
+    """Fonction pour récupérer l'ensemble des catégories"""
+    url = getdata("https://books.toscrape.com/index.html")
+    list_categories = []
+    if getdata.status_code == requests.codes.ok:
+    ul_cat = url.find_all("div", class_="side_categories").ul.ul
+    a_cat = url.find("a")
+       for cat in side_categories.find_all('a')["href"][1:]:
+            get_categories_data.append('https://books.toscrape.com/')
+        return get_categories_data
+    print(get_categories_data)
+
+
+
+def get_url_books(pages_parse):
+    """Fonction pour récupérer l'ensemble des livres"""
+    return
 
 
 def get_book_data(soup):
     """Fonction pour analyser les données d'un livre"""
     print("Démarrage du scrapping")
-
+    page = BeautifulSoup(soup.content, "html.parser")
     tds = soup.find_all(["td"])
-
     title = soup.find("h1")
-    print(title.text)
-
     upc = tds[0]
-    print(upc.text)
-
     price_excluding_tax = tds[2]
-    print(price_excluding_tax.text)
-
     price_including_tax = tds[3]
-    print(price_including_tax.text)
-
     number_available = tds[5]
-    print(number_available.text)
-
     description = soup.find("article").find_all(
         "p")[3].get_text().replace(";", "/").strip()
-    print(description)
-
     category = soup.find("ul", {"class": "breadcrumb"}).find_all("li")[2]
-    print(category.text)
-
     rating = soup.find(
         "div", {"class": "col-sm-6 product_main"}).find_all("p")[2]["class"][1]
-    print(rating)
-
-    images = soup.find('img')
-    name_img = images['alt']
-    link_img = images['src']
-    print(name_img, link_img)
-
+    images = soup.find('img')['src']
+    product_list = {
+        'title': title.text,
+        'category': category.text,
+        'description': description,
+        'rating': rating,
+        'price_et': price_excluding_tax.text,
+        'price_it': price_including_tax.text,
+        'upc': upc.text,
+        'stock': number_available.text,
+        'img': images,
+        'url': page, }
     print("Opération terminé")
+
 
 
 get_book_data(soup)
