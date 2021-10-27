fb55c94cd39bef591dbb503983ac99df62ea28e8
erikia <91466871+erikia@users.noreply.github.com>
2021-10-27 14:23:20 +0200

    dernier eversion

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from csv import DictWriter
 from pathlib import Path
 
 
-#url = ("http://books.toscrape.com/")
+url = ("http://books.toscrape.com/")
 IMG_DIR = "data/img/"
 CSV_DIR = "data/csv/"
 
@@ -16,45 +16,47 @@ def category_url(url_first):
         soup = BeautifulSoup(page.content, 'html.parser')
         side_categories = soup.find('div', class_='side_categories')
         category_urls = []
-        for _ in side_categories.find_all('a')[1:]:
-            category_urls.append('https://books.toscrape.com/' + _.get('href'))
+        for i in side_categories.find_all('a')[1:]:
+            category_urls.append('https://books.toscrape.com/' + i.get('href'))
+        print(category_urls)
         return category_urls
 
 
-def category_urls_next(category_urls, urls_books):
+def category_urls_next(category_urls):
     """Fonction pour récupérer l'ensemble des urls des  livres"""
     urls_books = []
-    request = requests.get(category_urls)
-    html = request.content
-    soup = BeautifulSoup(html, features="html.parser")
-    soup_books = soup.find_all("article", "product_pod")
+    url = 'http://books.toscrape.com/index.html'
+    request = requests.get(url)
+    if not request.ok:
+        print("erreur")
+        return
 
-    category = soup.find("h1").get_text()
-
-    for i in soup_books:
-        url_book = i.a["href"].replace("../", "")
+    soup = BeautifulSoup(request.content, 'html.parser')
+    for category_url in category_urls:
+        soup_books = soup.find_all("article", "product_pod")
+        category = soup.find("h3").get_text()
+        url_book = soup.a["href"].replace("../", "")
         urls_books.append(url_book)
-
     next_page = soup.find("li", "next")
-    url = url.replace("index.html", "")
+    urls = category_url.replace("index.html", "")
     while next_page:
-        url_next_page = url + next_page.a["href"]
+        url_next_page = urls + next_page.a["href"]
         request = requests.get(url_next_page)
         html = request.content
         soup = BeautifulSoup(html, features="html.parser")
         soup_books = soup.find_all("article", "product_pod")
-        for i in soup_books:
-            url_book = i.a["href"].replace("../", "")
+        for category_url in soup_books:
+            url_book = category_url.a["href"].replace("../", "")
             urls_books.append(url_book)
-        next_page = soup.find("li", "next")
-    print(urls_books, category)
-    return urls_books, category
+            next_page = soup.find("li", "next")
+        print(urls_books, category)
+        return urls_books, category
 
 
 def get_book_data(soup):
     """Fonction pour analyser les données d'un livre"""
     soup = BeautifulSoup(soup.content, "html.parser")
-    page = category_urls_next(urls_books)
+    page = url
     tds = soup.find_all(["td"])
     title = soup.find("h1")
     upc = tds[0]
@@ -83,34 +85,20 @@ def get_book_data(soup):
     return product_list
 
 
-def export_csv(filesdata):
+def export_csv(books, category):
     """Fonction pour exporter le ficher en csv"""
-    with open("data.csv", "w") as csvfile:
-        data_names = [
-            'product_page_url',
-            'title',
-            'upc',
-            'price_including_tax',
-            'price_excluding_tax',
-            'number_available',
-            'review_rating',
-            'product_description',
-            'category',
-            'url_image',
-        ]
-        writer = csv.DictWriter(csvfile, delimiter='|', filenames=data_names)
+    with open(f"csv/{category}.csv", "w", newline="") as csvfile:
+        writer = csv.DictWriter(
+            csvfile, fieldnames=list(vars(books[0]).keys()))
         writer.writeheader()
-        for data in filesdata:
-            writer.writerow(data)
-    print(filesdata)
+        for book in books:
+            writer.writerow(vars(book))
 
 
 def main():
-    url = 'https://books.toscrape.com/index.html'
     a = category_url(url)
     b = category_urls_next(a)
-    c = get_book_data(b)
-    print(c)
+    print(b)
 
 
 if __name__ == '__main__':
